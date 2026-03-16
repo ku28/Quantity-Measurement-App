ef2a8a31c5875c9ef81411c5f8f3f99228a14e1b
ku28 <ku28sh07@gmail.com>
2026-03-16 11:01:49 +0530

    InchEquality Done

--- QuantityMeasurementApp.java
+++ QuantityMeasurementApp.java
@@ -1,29 +1,68 @@
-import java.util.*;
-
-class QuantityMeasurementApp{
-    public static class Feet{
+public class QuantityMeasurementApp {
+    public static class Feet {
         private final double value;
-        public Feet(double value){
-            this.value =value;
-        } 
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
         @Override
-        public boolean equals(Object obj){
-            if(this==obj) return true;
-            if(obj==null || getClass()!=obj.getClass()) return false;
-            Feet temp=(Feet) obj;
-            return Double.compare(this.value, temp.value)==0;
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null || getClass() != obj.getClass())
+                return false;
+            Feet temp = (Feet) obj;
+            return Double.compare(this.value, temp.value) == 0;
         }
     }
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        double val1=sc.nextDouble();
-        double val2=sc.nextDouble();
 
-        QuantityMeasurementApp.Feet obj1=new QuantityMeasurementApp.Feet(val1);
-        QuantityMeasurementApp.Feet obj2=new QuantityMeasurementApp.Feet(val2);
+    public static class Inch {
+        private final double value;
 
-        if(obj1.equals(obj2)) System.out.println("Both are equal in feet");
-        else System.out.println("Values are different");
-        sc.close();
+        public Inch(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null || getClass() != obj.getClass())
+                return false;
+            Inch temp = (Inch) obj;
+            return Double.compare(this.value, temp.value) == 0;
+        }
+    }
+
+    public static void feetEquality() {
+        double val1 = 6.7;
+        double val2 = 6.7;
+
+        Feet obj1 = new Feet(val1);
+        Feet obj2 = new Feet(val2);
+
+        if (obj1.equals(obj2))
+            System.out.println("Both are equal in feet");
+        else
+            System.out.println("Values are different");
+    }
+
+    public static void inchEquality() {
+        double val1 = 10.0;
+        double val2 = 10.0;
+
+        Inch obj1 = new Inch(val1);
+        Inch obj2 = new Inch(val2);
+
+        if (obj1.equals(obj2))
+            System.out.println("Both are equal in inches");
+        else
+            System.out.println("Values are different");
+    }
+
+    public static void main(String[] args) {
+        feetEquality();
+        inchEquality();
     }
 }
